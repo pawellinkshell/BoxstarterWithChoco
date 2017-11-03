8852567197d57a2d9bff4afbf8add2cf67b149c1
Jan Koszela <jan.koszela@eclipsegroup.co.uk>
2017-11-03 11:49:08 +0100

    Add IntelliJ community edition to installation and upgrade process

--- Install/11_AdvanceIDETools.bat
+++ Install/11_AdvanceIDETools.bat
@@ -1,13 +1,17 @@
 @echo off
 
+echo.
 echo Start of --11_AdvanceIDETools.bat--
 echo.
 
-echo "1) Instal eclipse"
+echo "1) Install eclipse"
 choco install eclipse
 
-echo "2) Instal intellijidea-ultimate"
+echo "2) Install intellijidea-ultimate"
 choco install intellijidea-ultimate
+
+echo "3) Install intellijidea-community
+choco install intellijidea-community
 
 echo End of --11_AdvanceIDETools.bat--
 echo.
--- Update/10_AdvanceIDETools.bat
+++ Update/10_AdvanceIDETools.bat
@@ -1,5 +1,6 @@
 @echo off
 
+echo.
 echo Start of --10_AdvanceIDETools.bat--
 echo.
 
@@ -8,6 +9,9 @@ choco upgrade eclipse
 
 echo "2) Upgrade intellijidea-ultimate"
 choco upgrade intellijidea-ultimate
+
+echo "3) Upgrade intellijidea-community
+choco upgrade intellijidea-community
 
 echo End of --10_AdvanceIDETools.bat--
 echo.
