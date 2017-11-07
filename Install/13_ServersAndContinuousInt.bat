005129f26a16978eb8859f15e05c4d31b6cefe6a
Jan Koszela <jan.koszela@eclipsegroup.co.uk>
2017-11-07 15:47:58 +0100

    Add docker kitematic and docker for windows

--- Install/13_ServersAndContinuousInt.bat
+++ Install/13_ServersAndContinuousInt.bat
@@ -3,14 +3,20 @@
 echo Start of --13_ServersAndContinuousInt.bat--
 echo.
 
-echo "1) Instal tomcat"
+echo "1) Install tomcat"
 choco install tomcat
 
-echo "2) Instal heroku-cli"
+echo "2) Install heroku-cli"
 choco install heroku-cli
 
-echo "3) Instal docker"
+echo "3) Install docker"
 choco install docker
+
+echo "4) Install docker-for-windows"
+choco install docker-for-windows
+
+echo "5) Install docker-kitematic"
+choco install docker-kitematic
 
 echo End of --13_ServersAndContinuousInt.bat--
 echo.
--- Update/12_ServersAndContinuousInt.bat
+++ Update/12_ServersAndContinuousInt.bat
@@ -12,5 +12,11 @@ choco upgrade heroku-cli
 echo "3) Upgrade docker"
 choco upgrade docker
 
+echo "4) Upgrade docker-for-windows"
+choco upgrade docker-for-windows
+
+echo "5) Upgrade docker-kitematic"
+choco upgrade docker-kitematic
+
 echo End of --12_ServersAndContinuousInt.bat--
 echo.
