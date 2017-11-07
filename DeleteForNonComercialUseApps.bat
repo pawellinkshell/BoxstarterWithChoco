5078b755beff16bebafae2d9027ea0e16e2fab82
Jan Koszela <jan.koszela@eclipsegroup.co.uk>
2017-11-07 15:50:19 +0100

    Add file to remove application which cannot be applied in commercial use

---
+++ DeleteForNonComercialUseApps.bat
@@ -0,0 +1,20 @@
+@echo off
+
+echo Start of --1_OnlyNonComercialApps.bat--
+echo.
+
+echo "1) Uninstall teamviewer"
+choco uninstall teamviewer
+
+REM echo "5) Uninstall ccleaner"
+REM choco uninstall ccleaner
+
+REM echo "6) Uninstall ccenhancer for CCleaner"
+REM choco uninstall ccenhancer
+
+REM echo "6) Uninstall teracopy"
+REM choco uninstall teracopy
+
+
+echo End of --1_OnlyNonComercialApps.bat--
+echo.
